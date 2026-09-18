b7db5bc8bde964a41fba19c90043ba0f25063c5f
kkasonic <kkasonic@asu.edu>
2026-09-18 18:13:37 -0400

    Finished Implementing Password Evaluation
    Added method updatePassword(username, password) in the Database
    Added Password Evaluation to User Update page

--- HW1/src/database/Database.java
+++ HW1/src/database/Database.java
@@ -1,10 +1,6 @@
 package database;
 
 import java.sql.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -865,6 +861,29 @@ public class Database {
 	    }
 	}
 	
+	/*******
+	 * <p> Method: void updatePassword(String username, String password) </p>
+	 * 
+	 * <p> Description: Update the password of a user given that user's username and the new
+	 *		password.</p>
+	 * 
+	 * @param username is the username of the user
+	 * 
+	 * @param password is the new password for the user
+	 *  
+	 */
+	// update the password
+	public void updatePassword(String username, String password) {
+	    String query = "UPDATE userDB SET password = ? WHERE username = ?";
+	    try (PreparedStatement pstmt = connection.prepareStatement(query)) {
+	        pstmt.setString(1, password);
+	        pstmt.setString(2, username);
+	        pstmt.executeUpdate();
+	        currentPassword = password;
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	}
 	
 	/*******
 	 * <p> Method: boolean getUserAccountDetails(String username) </p>
